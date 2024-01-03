2e9ede1c270e788c3aa47172e38d2d91f15e5e1f
Reagan <mamesareagan0@gmail.com>
2024-01-03 18:03:04 +0300

    return every employee name in a string, separated by |

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+
+  for (const employee of reportWithIterator) {
+    result += `${employee} | `;
+  }
+  result = result.slice(0, -3);
+
+  return result;
+}
